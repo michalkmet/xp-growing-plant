e00cd44234ee9f78b67aaf4f64f68e2a2b53b839
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-07-03 12:34:02 +0200

    4th test pass

--- src/growingPlant.js
+++ src/growingPlant.js
@@ -1,5 +1,15 @@
 function growingPlant(upSpeed, downSpeed,desiredHeight) {
-  throw new Error('not valid input');
+  if(
+    typeof upSpeed != 'number' || 
+    typeof downSpeed != 'number' ||  
+    typeof desiredHeight != 'number' ||
+    upSpeed < 1 ||
+    downSpeed < 1 ||
+    desiredHeight < 1
+    ){
+      throw new Error('not valid input');
+    }
+    return 1;
 }
 
 module.exports = growingPlant;
